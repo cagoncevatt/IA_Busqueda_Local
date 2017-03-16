1b2c165173aba652fc73ac946a70dad9ee64ff28
Alejandro Goncevatt <ca@goncevatt.com>
2017-03-16 04:09:55 +0100

    Base Project Creation

---
+++ IA_Practica_1_Busqueda_Local/src/ia_practica_1_busqueda_local/IA_Practica_1_Busqueda_Local.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ia_practica_1_busqueda_local;
+
+/**
+ *
+ * @author Ale
+ */
+public class IA_Practica_1_Busqueda_Local {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
